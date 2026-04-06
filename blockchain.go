4772631c7c7b7b5354fc7042e9b8f49e7ebc1241
Raiden-2200771 <raiden.2200771@gmail.com>
2026-04-06 22:07:54 +0900

    feat: Blockchainの実装とテストを追加

---
+++ blockchain.go
@@ -0,0 +1,25 @@
+package main
+
+import "time"
+
+type Blockchain struct {
+	Blocks []Block
+}
+
+func newBlockchain() Blockchain {
+	genesis := Block{
+		Index:     0,
+		Timestamp: time.Now().Format(time.RFC3339),
+		Data:      "Genesis Block",
+		PrevHash:  "",
+	}
+	genesis.Hash = hash(genesis)
+
+	return Blockchain{Blocks: []Block{genesis}}
+}
+
+func (bc *Blockchain) addBlock(data string) {
+	prev := bc.Blocks[len(bc.Blocks)-1]
+	b := newBlock(prev, data)
+	bc.Blocks = append(bc.Blocks, b)
+}
---
+++ blockchain_test.go
@@ -0,0 +1,56 @@
+package main
+
+import "testing"
+
+func TestNewBlockchain_HashGenesisBlock(t *testing.T) {
+	bc := newBlockchain()
+
+	if len(bc.Blocks) != 1 {
+		t.Errorf("ブロック数が正しくありません: got %d, want 1", len(bc.Blocks))
+	}
+}
+
+func TestNewBlockchain_GenesisIndex(t *testing.T) {
+	bc := newBlockchain()
+
+	if bc.Blocks[0].Index != 0 {
+		t.Errorf("GenesisブロックのIndexが正しくありません: got %d, want 0", bc.Blocks[0].Index)
+	}
+}
+
+func TestAddBlock_Length(t *testing.T) {
+	bc := newBlockchain()
+
+	bc.addBlock("田中議員が〇〇法案に賛成票を投じた")
+
+	if len(bc.Blocks) != 2 {
+		t.Errorf("ブロック数が正しくありません: got %d, want 2", len(bc.Blocks))
+	}
+}
+
+func TestAddBlock_PrevHash(t *testing.T) {
+	// Arrange
+	bc := newBlockchain()
+
+	// Act
+	bc.addBlock("田中議員が〇〇法案に賛成票を投じた")
+
+	// Assert
+	want := bc.Blocks[0].Hash
+	got := bc.Blocks[1].PrevHash
+	if got != want {
+		t.Errorf("PrevHashが正しくありません: got %s, want %s", got, want)
+	}
+}
+
+func TestAddBlock_Data(t *testing.T) {
+	bc := newBlockchain()
+
+	bc.addBlock("田中議員が〇〇法案に賛成票を投じた")
+
+	want := "田中議員が〇〇法案に賛成票を投じた"
+	got := bc.Blocks[1].Data
+	if got != want {
+		t.Errorf("Dataが正しくありません: got %s, want %s", got, want)
+	}
+}
