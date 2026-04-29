8d1f7d6405dd3c20f90f23e29a1ef63b02221086
Raiden-2200771 <raiden.2200771@gmail.com>
2026-04-29 13:21:03 +0900

    feat: ポートをコマンドライン引数で指定できるようにする

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 )
 
 func main() {
+	port := flag.String("port", "8080", "ポート番号")
+	flag.Parse()
+
 	http.HandleFunc("/chain", chainHandler)
 	http.HandleFunc("/block", blockHandler)
-	fmt.Println("Listening on :8080")
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("Listening on :" + *port)
+	http.ListenAndServe(":"+*port, nil)
 }
