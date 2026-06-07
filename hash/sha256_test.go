13f54f9172d0ef3c6e7b08a32f8844e72f559d1f
Raiden-2200771 <raiden.2200771@gmail.com>
2026-06-07 17:01:33 +0900

    refactor: TestString_ReturnsSHA256Hash を TestNew_ReturnsValidSHA256 に統合

--- hash/sha256_test.go
+++ hash/sha256_test.go
@@ -16,6 +16,24 @@ func TestNew_SameDataReturnsSameHash(t *testing.T) {
 	}
 }
 
+func TestNew_ReturnsValidSHA256(t *testing.T) {
+	// value は非公開のため String() 経由でのみ検証できる
+	h := hash.New("テスト公約")
+
+	s := h.String()
+
+	if len(s) != 64 {
+		t.Errorf("New()の戻り値が64文字ではありません: got %d", len(s))
+	}
+	for _, c := range s {
+		if !('0' <= c && c <= '9') && !('a' <= c && c <= 'f') {
+			t.Errorf("New()の戻り値に16進数以外の文字が含まれています: %c", c)
+		}
+	}
+}
+
+// func TestString_ReturnsSHA256Hash は TestNew_ReturnsValidSHA256 で兼ねているため省略
+
 func TestNew_DifferentDataReturnsDifferentHash(t *testing.T) {
 	h1 := hash.New("テスト公約")
 	h2 := hash.New("別のテスト公約")
@@ -63,17 +81,3 @@ func TestZero_Returns64Zeros(t *testing.T) {
 	}
 }
 
-func TestString_ReturnsSHA256Hash(t *testing.T) {
-	h := hash.New("テスト公約")
-
-	s := h.String()
-
-	if len(s) != 64 {
-		t.Errorf("String()の長さが不正です: got %d, want 64", len(s))
-	}
-	for _, c := range s {
-		if !('0' <= c && c <= '9') && !('a' <= c && c <= 'f') {
-			t.Errorf("String()に16進数以外の文字が含まれています: %c", c)
-		}
-	}
-}
